edf4664d160db99f5181fbdd7e25484ac5638edb
brayan usme <cuentaparavisuall@outlook.com>
2024-11-28 16:11:36 -0500

    guardar

--- practicasdepoo/practicasdepoo/Program.cs
+++ practicasdepoo/practicasdepoo/Program.cs
@@ -1,17 +1,28 @@
-﻿internal class Program
+﻿using System.Timers;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
-        
-        
-        Console.WriteLine("Hello, World! como va la peoople en esa monda ");
 
-        Console.WriteLine("ingresame esa monda vale mia ");
-       string sobelo = Console.ReadLine();
 
-        Console.WriteLine( " el texto que escribistee fue \n" + sobelo);
 
+
+
+        //hacer un codigo que pida dos numeros y luego lo sume
+
+        Console.WriteLine("ingrese el primer numero");
+       double num1= Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" EL NUMERO INGRESADO FUE " + num1);
+
+
+        Console.WriteLine("ingrese el SEGUNDO numero");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" EL NUMERO INGRESADO FUE " + num2);
+
+        double opereacion = num1 + num2;
+
+        Console.WriteLine(" el resultado de la suma es " + opereacion);
 
 
 
