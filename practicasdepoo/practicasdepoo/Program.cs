ed45da7e5bc6d8408f74df554e56d891fc887b5d
brayan usme <cuentaparavisuall@outlook.com>
2024-11-28 15:11:23 -0500

    sobelo bien ricooooo#

---
+++ practicasdepoo/practicasdepoo/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+        
+        Console.WriteLine("Hello, World! como va la peoople en esa monda ");
+
+        Console.WriteLine("ingresame esa monda vale mia ");
+       string sobelo = Console.ReadLine();
+
+        Console.WriteLine( " el texto que escribistee fue \n" + sobelo);
+
+
+
+
+
+
+
+
+    }
+}
