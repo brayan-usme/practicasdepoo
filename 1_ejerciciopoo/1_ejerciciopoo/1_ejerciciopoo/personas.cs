904001340e7ec11862522be5aada323eb841b4e8
brayan usme <cuentaparavisuall@outlook.com>
2024-11-28 17:46:53 -0500

    sobelo

---
+++ 1_ejerciciopoo/1_ejerciciopoo/1_ejerciciopoo/personas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ejerciciopoo
+{
+    internal class personas
+    {
+        //propiedades de mi clase personas
+
+        public string Nombre { get; set; } // FORMA DE DEFINIR PROPIEDADES
+        public int Edad { get; set; } // get =  Se utiliza cuando necesitas leer el valor de un campo.
+                                      // set = Se utiliza cuando necesitas escribir o actualizar el valor de un campo.
+
+
+        // METODOS DE MI CLASE PERSONA 
+
+        public void Presentar() // FORMA DE CREAR UN METODO EN C# :
+                                // = MODIFICADOR DE ACCESO + TIPO DE DATO QUE RETORNA + NOMBRE DEL METODO + SECCION DE PARAMETROS + CORCHETES FINALES
+        {
+            Console.WriteLine($" Hola, mí nombre es {Nombre} y tengo {Edad}");
+
+        }
+
+        public void MayorEdad()
+        {
+
+            if (Edad >= 18)
+            {
+
+                Console.WriteLine(" Soy mayor de edad y ya me puedo endeudar ");
+
+            }
+            else 
+            {
+             Console.WriteLine(" Soy menor de edad y no me puedo endeudar ");
+            } 
+        }
+
+
+    }
+}
