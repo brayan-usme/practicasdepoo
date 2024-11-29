20cbc1704c22814161e364e23a48a05441d0bc02
brayan usme <cuentaparavisuall@outlook.com>
2024-11-28 19:20:38 -0500

    todo el ejercicio de como crear objetos en poo

--- 1_ejerciciopoo/1_ejerciciopoo/1_ejerciciopoo/Program.cs
+++ 1_ejerciciopoo/1_ejerciciopoo/1_ejerciciopoo/Program.cs
@@ -1,9 +1,55 @@
-﻿internal class Program
+﻿using _1_ejerciciopoo;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
+        // creacion de un objeto 
+
+        // vamos a preguntar por pantalla cuantas personas vamos a presentar
+
+        Console.WriteLine(" ¿Cuantas personas quieres presentar ? ");
+        
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+
+        {
+                                                 // FORMA LARGA  DE INICIALIZAR 
+
+           /* Console.WriteLine(" INGRESE EL NOMBRE DE LA PERSONA");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine(" INGRESE LA EDAD DE LA PERSONA");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            // ( vamos a crear una nueva isntacia de la clase personas es decir un nuevo objeto)
+
+            personas objeto_personas = new personas();
+
+            // seteando propiedades
+
+            objeto_personas.Nombre = nombre;
+
+            objeto_personas.Edad = edad;
+           */
+
+
+                                                        // FROMA CORTA
+
+            personas objeto_personas = new personas(); // SE CREA PRIMERO EL OBJETO Y DESPUES SE INSTANCIA  Y DEBE  IR FUERA DEL FOR ES DECIR CREAR AL PRINCIPIO DEL PRGRAMA
+
+            Console.WriteLine(" INGRESE EL NOMBRE DE LA PERSONA  \n");
+            objeto_personas.Nombre = Console.ReadLine();
+
+            Console.WriteLine(" INGRESE LA EDAD DE LA PERSONA\n ");
+            objeto_personas.Edad = Convert.ToInt32(Console.ReadLine());
+
+            // 2 INVOCAR METODOS DE LAS CLASES PERSONAS
+            objeto_personas.Presentar();
+            objeto_personas.MayorEdad();
 
 
 
@@ -21,37 +67,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
 
 
--- 1_ejerciciopoo/1_ejerciciopoo/1_ejerciciopoo/personas.cs
+++ 1_ejerciciopoo/1_ejerciciopoo/1_ejerciciopoo/personas.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace _1_ejerciciopoo
 {
-    internal class personas
+    public class personas
     {
         //propiedades de mi clase personas
 
         public string Nombre { get; set; } // FORMA DE DEFINIR PROPIEDADES
         public int Edad { get; set; } // get =  Se utiliza cuando necesitas leer el valor de un campo.
                                       // set = Se utiliza cuando necesitas escribir o actualizar el valor de un campo.
-
+        private double Salario { get; set; }
 
         // METODOS DE MI CLASE PERSONA 
 
         public void Presentar() // FORMA DE CREAR UN METODO EN C# :
                                 // = MODIFICADOR DE ACCESO + TIPO DE DATO QUE RETORNA + NOMBRE DEL METODO + SECCION DE PARAMETROS + CORCHETES FINALES
         {
-            Console.WriteLine($" Hola, mí nombre es {Nombre} y tengo {Edad}");
+            Console.WriteLine($" Hola, mí nombre es {Nombre} \n tengo {Edad} \n");
 
         }
 
@@ -30,12 +30,12 @@ namespace _1_ejerciciopoo
             if (Edad >= 18)
             {
 
-                Console.WriteLine(" Soy mayor de edad y ya me puedo endeudar ");
+                Console.WriteLine(" Soy mayor de edad y ya me puedo endeudar\n ");
 
             }
             else 
             {
-             Console.WriteLine(" Soy menor de edad y no me puedo endeudar ");
+             Console.WriteLine(" Soy menor de edad y no me puedo endeudar\n ");
             } 
         }
 
