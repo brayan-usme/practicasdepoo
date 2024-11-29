462219304d4afe96718425a85e425670a45bb678
brayan usme <cuentaparavisuall@outlook.com>
2024-11-29 17:00:29 -0500

    ACTU1

--- EJERCICIO_POO_MAS_ARRAY/EJERCICIO_POO_MAS_ARRAY/EJERCICIO_POO_MAS_ARRAY/Program.cs
+++ EJERCICIO_POO_MAS_ARRAY/EJERCICIO_POO_MAS_ARRAY/EJERCICIO_POO_MAS_ARRAY/Program.cs
@@ -2,13 +2,43 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] MESESAÑO = new string[] { "enero", "febrero", "marzo", "abril", "marzo", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "novienbre", "diciembre" };
+
+        decimal[] sales = new decimal[12];
+
+        decimal suma = 0;
+
+
+        // metodo almacenar ventas
+        Almacenarventas();
+
+
+        //METODO CALCULAR VENTAS DEL AÑO
+        Calcularventas();
+        //METODO PARA MOSTRAR VENTAS
+        Mostrarventas();
+
+
 
 
     }
 
 
-    
+
+    private static void Almacenarventas()
+    {
+
+
+    }
+
+    private static void Calcularventas()
+    {
+
+
+    }
+
+    private static void Mostrarventas()
+    {
 
 
 
@@ -17,5 +47,4 @@
 
 
 
-
-}
+    }
